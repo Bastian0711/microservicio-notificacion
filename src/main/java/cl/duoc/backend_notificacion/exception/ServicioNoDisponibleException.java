0d3318616c775c70ed32a749023be6027aa59487
Bastian0711 <bastiankisto@gmail.com>
2026-07-05 23:49:20 -0400

    Delete

--- src/main/java/cl/duoc/backend_notificacion/exception/ServicioNoDisponibleException.java
+++
@@ -1,8 +0,0 @@
-package cl.duoc.backend_notificacion.exception;
-
-public class ServicioNoDisponibleException extends RuntimeException {
-
-    public ServicioNoDisponibleException(String mensaje) {
-        super(mensaje);
-    }
-}
